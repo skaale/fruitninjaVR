5ebf2a83418924d921051968e26df121dba13734
skaale35 <simulacrumdk@gmail.com>
2021-05-31 08:31:27 +0200

    ADD SOURCE FILES

---
+++ Assets/Scripts/GameOverIfFruitTouchGround.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverIfFruitTouchGround : MonoBehaviour
+{
+    public int maxNumberOfError = 3;
+    private int currentNumberOfError = 0;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.layer == 8)
+        {
+            currentNumberOfError++;
+            if(currentNumberOfError == maxNumberOfError)
+            {
+                UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+            }
+
+            Destroy(collision.gameObject);
+        }
+    }
+}
